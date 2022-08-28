c1d316ede6ecd863e9beef4c44f7e9781e12f8c5
yaitox <neneuug@hotmail.com>
2022-08-28 09:47:11 -0500

    Main cleanup

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.insert(0, 'D:\Pseudorandom-Generator\PseudorandomGenerator\src\Generators')
 
-from generator import ProductosMedios, CuadradosMedios, MultiplicadorConstante, Lineal, GenerateRandomNumbers, GeneratorType
+from generator import GenerateRandomNumbers, GeneratorType
 
 if __name__ == '__main__':
     numbers = GenerateRandomNumbers(GeneratorType.GENERATOR_MULTIPLICADOR_CONSTANTE)
